74e802cc0664f5c89ee385fc1c5d9f62d57145bf
Jukka Kaartinen <jukka.kaartinen@unikie.com>
2020-11-12 14:59:54 +0200

    Initial setup.sh
    Signed-off-by: Jukka Kaartinen <jukka.kaartinen@unikie.com>

---
+++ setup.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+#set -x
+
+CCACHE=
+
+if ! [ -x "$(command -v ccache)" ]; then
+    echo 'Info: ccache is not installed.' >&2
+else
+    CCACHE="ccache"
+    export CCACHE_DIR=`pwd`/.ccache
+    export CCACHE_MAXSIZE=10G
+
+    ccache -s
+fi
+
+MNT_DIR=`pwd`/mnt
+
+
+function sdcard {
+    mkdir -p $MNT_DIR
+    mkdir -p $MNT_DIR/fat32
+    mkdir -p $MNT_DIR/ext4
+    sudo mount /dev/sda1 $MNT_DIR/fat32
+    sudo mount /dev/sda2 $MNT_DIR/ext4
+}
+
+function clone {
+    git clone git@github.com:raspberrypi/linux.git
+    pushd linux
+    git checkout -b origin/rpi-5.9.y
+    popd
+    #cp ../ubuntu_20.10-config-5.8.0-1007-raspi .config
+    cp ubuntu_20.10-config-5.8.0-1007-raspi linux/arch/arm64/configs/ubuntu2010_defconfig
+}
+
+function compile {
+    pushd linux
+
+    # RUN in docker
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- ubuntu2010_defconfig
+    make -j 8 ARCH=arm64 CROSS_COMPILE="${CCACHE} aarch64-linux-gnu-" Image modules dtbs
+    make ARCH=arm64 CROSS_COMPILE="${CCACHE} aarch64-linux-gnu-" INSTALL_MOD_PATH=../images/modules modules_install
+    make ARCH=arm64 CROSS_COMPILE="${CCACHE} aarch64-linux-gnu-" INSTALL_PATH=../images/boot install
+    # RUN in docker end 
+
+    popd # linux
+}
+
+function update {
+    echo "Copying libs.."
+    cp -r images/modules/lib/* $MNT_DIR/ext4/lib/
+
+    echo "Copying dtbs.."
+    #cp linux/arch/arm64/boot/dts/broadcom/*.dtb $MNT_DIR/fat32/
+    #cp linux/arch/arm64/boot/dts/overlays/*.dtb* $MNT_DIR/fat32/overlays/
+    cp linux/arch/arm64/boot/dts/overlays/README $MNT_DIR/fat32/overlays/
+    echo "Copying vmlinuz"
+    cp images/boot/vmlinuz-5.9.6+ $MNT_DIR/fat32/vmlinuz
+
+    cp images/boot/* $MNT_DIR/ext4/boot/
+    echo "Syncing.."
+
+set -x
+    for i in dev proc sys dev/pts
+    do
+        mount -o bind /$i $MNT_DIR/ext4/$i
+    done
+cat << EOF | chroot $MNT_DIR/ext4
+set -x
+#update-initramfs -c -t -k "5.9.6+"
+EOF
+#    
+
+    for i in dev/pts proc sys dev
+    do
+        umount $MNT_DIR/ext4/$i
+    done
+    sync
+}
+
+
+function usdcard {
+    sudo umount $MNT_DIR/fat32
+    sudo umount $MNT_DIR/ext4
+}
+
+$1

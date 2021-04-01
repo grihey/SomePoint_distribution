f824924f579be1615165b62252e016a13398666b
Tero Tervala <tero.tervala@unikie.com>
2021-04-01 14:30:27 +0300

    Add script for installing our git hooks
    install-hooks.sh will fetch our version of commit-msg and
    check-commit.sh scripts from Phone/misc and place them in .git/hooks.
    It will also link these hooks for the subrepos that are in our Gerrit.
    
    Jira-Id: SP-379
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>
    Depends-On: I18c886d14c147d16a236934f126e3e4aab84c798
    Change-Id: I6ca2152d90d3e9e0eebb107831ead2e00ed1fbce

---
+++ install-hooks.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SCRIPTS=(check-commit.sh commit-msg)
+SUBREPOS=(docker linux flutter-shim)
+
+set -e
+
+if [ ! -d .git/hooks ]; then
+    echo ".git/hooks not found" >&2
+    exit 1
+fi
+
+# Get repo url with usernames and ports etc.
+REPO="$(git remote get-url --all origin)"
+REPO="${REPO%%sp_distro}"
+REPO+="misc"
+
+# Fetch scripts from Phone/misc/githooks
+for S in "${SCRIPTS[@]}"; do
+    echo "Fetching ${S}"
+    git archive "--remote=${REPO}" HEAD "githooks/${S}" | tar xO > ".git/hooks/${S}"
+    chmod a+x ".git/hooks/${S}"
+done
+
+# Link main repo hooks to our subrepos
+for R in "${SUBREPOS[@]}"; do
+    if [ -d ".git/modules/${R}" ]; then
+        echo "Removing .git/modules/${R}/hooks"
+        rm -rf ".git/modules/${R}/hooks"
+        echo "Linking .git/modules/${R}/hooks to .git/hooks"
+        ln -s ../../hooks ".git/modules/${R}/hooks"
+    else
+        echo ".git/modules/${R} not found!" >&2
+        exit 1
+    fi
+done
